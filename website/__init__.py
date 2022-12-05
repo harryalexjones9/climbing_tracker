ca046e231cdecb51acc6fd9c28bc68ee64c5b82a
Harry Jones <harryalexjones@yahoo.co.uk>
2022-12-05 17:37:38 +0000

    Start work on Session database

--- website/__init__.py
+++ website/__init__.py
@@ -18,7 +18,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User, Note
+    from .models import User, Note, Session
 
     create_database(app)
 
@@ -33,6 +33,10 @@ def create_app():
     return app
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    # if not path.exists('website/' + DB_NAME):
+    #     db.create_all(app=app)
+    #     print('Created Database!')
+
+    with app.app_context():
         db.create_all(app=app)
         print('Created Database!')
--- website/views.py
+++ website/views.py
@@ -1,9 +1,27 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 from flask_login import login_required, current_user
+from .models import Note, Session
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+
+    if request.method == 'POST':
+        note = request.form.get('note')
+
+        # new_note = Note(data=note, user_id=current_user.id)
+        # db.session.add(new_note)
+        # db.session.commit()
+
+        session = request.form.get('session')
+
+        new_session = Session(v0=session, user_id=current_user.id)
+        db.session.add(new_session)
+        db.session.commit()
+
+        flash('Session Added!', category='success')
+
     return render_template("home.html", user=current_user)
